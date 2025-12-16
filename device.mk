ad197b466bf92d2614d1a2b6ed17656b05b03bf2
voidxog <ahmedarefinsajjad@gmail.com>
2025-12-16 21:46:03 +0600

    sunny: Switch to ext4 from erofs
    * EROFS is great but it doesn't allow flashing gapps or other essential recovery flashables hence the switch to EXT4
    
    Signed-off-by: voidxog <ahmedarefinsajjad@gmail.com>

--- device.mk
+++ device.mk
@@ -20,13 +20,11 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
